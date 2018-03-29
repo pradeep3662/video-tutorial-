bdcd57f3c9913ce06cb14b3abc52bf286b7f8ef6
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-03-29 17:18:07 +0530

    addition

---
+++ Diffaddition.java
@@ -0,0 +1,25 @@
+package javaPrgusingfunctions;
+
+public class Diffaddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Diffaddition obj=new Diffaddition();
+		
+		int p=obj.pqr();
+		System.out.println(p);
+	}
+		
+		
+		public int pqr(){
+			System.out.println("pqr method");
+			int a=10,b=20;
+			int c=a+b;
+			return c;
+			
+		}
+		
+		
+	
+
+}
