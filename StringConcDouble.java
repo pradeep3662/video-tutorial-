b176f02b0b34b279104da32054e205d15febbcf0
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-03-29 12:26:31 +0530

    new code on stringconcatenation

---
+++ StringConcDouble.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+
+public class StringConcDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                  
+		double c =12.33;
+		double d =34.43;
+		
+		System.out.println(c+d);
+		
+		System.out.println("hello world");
+		System.out.println("the value of a :"+c);
+		
+        System.out.print("hello testing");	
+        System.out.println("my name is pradeep");
+	}
+
+}
