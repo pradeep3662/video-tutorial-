cc9e7b657f652ebc733ca67d7d5dc80d3e3fa556
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-03-29 20:57:01 +0530

    while loop

---
+++ Whileprg.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class Whileprg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// while loop
+		int i=1;
+		while(i<=10){
+			System.out.println(i);
+			i=i+1;
+		}
+		//for loop ,j++=J=1;
+		// loop
+		for(int j=1;j==10;j++){
+			System.out.println(j);
+			
+		}
+		
+		
+		
+
+	}
+
+}
