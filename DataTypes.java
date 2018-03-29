e05ca071f2a145073f2e052909afc22c2d1de5a3
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-03-29 11:25:41 +0530

    Added new project

---
+++ DataTypes.java
@@ -0,0 +1,36 @@
+package JavaBasics;
+
+public class DataTypes {
+	//this is my first java code 
+	
+	/*this is 
+	my first
+	java code*/
+//main method....>starting of the program
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+// int;
+		int i=10;
+		i=20;
+		int j=30;
+		System.out.println(j);
+		int k=40;
+		System.out.println(k);
+		
+		// double 
+		double d =12.33;
+		double d1 =34.44;
+		System.out.println(d1);
+		
+		//char.... only single digit value 
+		char c='a';
+		char c1 = 'A';
+		char c2 = '1';
+		System.out.println(c2);
+		
+	}
+
+}
