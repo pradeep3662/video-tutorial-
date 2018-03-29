d8e3769e93bb54a6a475ee54fb8b4e17e3572cba
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-03-29 16:51:10 +0530

    newadditon program got output

---
+++ newadditions.java
@@ -0,0 +1,29 @@
+package javaPrgusingfunctions;
+
+public class newadditions{
+	int a;
+	int b;
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 newadditions add=new newadditions();
+		add.a=10;
+		add.b=20;
+		System.out.println(add.a);
+		System.out.println(add.b);
+		add.addit();
+		
+	}
+	public void addit(){
+		int c=a+b;
+		System.out.println(c);
+		
+	}
+	
+		
+		
+		
+
+	}
