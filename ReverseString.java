aabdcc926401c548f071f1f487e2e9a40e9ee8d4
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-04-04 21:48:06 +0530

    reverse string

---
+++ ReverseString.java
@@ -0,0 +1,28 @@
+package samplePrograms;
+
+import java.util.Scanner;
+
+
+
+public class ReverseString {
+	public static void main(String[] args)
+	{
+		String original;
+		String rev ="";
+		
+System.out.println("Enter String to reverse: ");
+Scanner sc = new Scanner(System.in);
+original = sc.nextLine();
+
+
+	int len = original.length();
+	for(int i =len-1;i>=0;i--){
+		rev=rev+original.charAt(i);
+	 
+		System.out.println("original string is:"+original);
+				// TODO Auto-generated constructor stub
+				System.out.println("reverse string :"+rev);
+	}
+	}
+}
+		
