8a7faabba49744c3082bae604b235d9b8cad957b
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-04-05 21:14:44 +0530

    got output

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,18 +1,18 @@
 package samplePrograms;
-import java.util.*;
+import java.util.Scanner;
 
 public class EvenOrOdd {
 
 	public static void main(String[] args) {
+		System.out.println("enter value");
 		// TODO Auto-generated method stub
-		int i1;
-		scanner i1=new scanner(System.in);
-		i1.nextint();
-		System.out.println("enter the value");
+	
+		Scanner sc = new Scanner(System.in);
 		
-		
-		if(i1/2==0){
-			System.out.println("even number");}
+		int i = sc.nextInt();
+
+		if(i%2==0)
+			System.out.println("even number");
 		else
 				System.out.println("odd number");
 		}}
