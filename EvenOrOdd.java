59ce215c2823ecdc44279840295610d459dadfa0
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-04-05 16:54:21 +0530

    even /odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,16 @@
+package samplePrograms;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=7;
+		if(i/2==0){
+			System.out.println("even number");}
+		else
+				System.out.println("odd number");
+		}}
+
+	
+
+
