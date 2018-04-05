3f5a1b8ca89187ef6649444c0a38eab605895d8c
pradeep3662 <37880496+pradeep3662@users.noreply.github.com>
2018-04-05 17:05:39 +0530

    confused plz check it

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,11 +1,17 @@
 package samplePrograms;
+import java.util.*;
 
 public class EvenOrOdd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i=7;
-		if(i/2==0){
+		int i1;
+		scanner i1=new scanner(System.in);
+		i1.nextint();
+		System.out.println("enter the value");
+		
+		
+		if(i1/2==0){
 			System.out.println("even number");}
 		else
 				System.out.println("odd number");
